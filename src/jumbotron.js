48c1289efadc671093e945c969c50088596b1c73
Armando Campos <camposa03@yahoo.com>
2020-01-29 16:50:09 -0700

    Adding mock github service

--- src/jumbotron.js
+++ src/jumbotron.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Jumbotron, Button, Row, Col } from 'reactstrap';
+import gitHubService from './services/GithubService';
 
-const Heading = (props) => {
-    return (
-        <div>
-            <Row>
-                <Col sm={{ size: 6, offset: 2 }}>
-                    <Jumbotron>
-                        <h1 className="display-3">Sample App</h1>
-                        <p className="lead">Some sample text.</p>
-                        <hr className="my-2" />
-                        <p>Random text here</p>
-                        <p className="lead">
-                            <Button color="primary">Learn More</Button>
-                        </p>
-                    </Jumbotron>
- 
-                     
-                </Col>
-            </Row>
-        </div>
-    );
-};
+class Heading extends Component {
+
+    state = {
+        bands: []
+    }
+
+    handleClick = () => {
+        const bands = gitHubService.getMockUserProfile();
+        this.setState({ bands});
+    }
+
+    render() {
+
+        const bands = this.state.bands;
+        const renderItems = () => {
+            return bands && bands.map((b, index) => <li key={index}>{b}</li>)
+        }
+
+        return (
+            <div>
+                <Row>
+                    <Col sm={{ size: 6, offset: 2 }}>
+                        <Jumbotron>
+                            <h1 className="display-3">Demo App</h1>
+                            <p className="lead"></p>
+                            <hr className="my-2" />
+                            <p>Click the button below to make an API call</p>
+                            <p className="lead">
+                                <Button onClick = {this.handleClick} color="primary">Get user Profile</Button>
+                            </p>
+                        </Jumbotron>
+
+                        { renderItems() }
+
+                    </Col>
+                </Row>
+            </div>);
+    }
+}
 
 export default Heading;
---
+++ src/services/GithubService.js
@@ -0,0 +1,13 @@
+const gitHubService = {
+
+    getMockUserProfile() {
+        return ["KOL", "Led Zeppelin", "RHCP", "Modest Mouse"];
+    },
+
+    getGithubProfile() {
+
+    }
+    
+};
+
+export default gitHubService;
